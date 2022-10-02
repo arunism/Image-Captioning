33a939e0e858bbe469f084ce01390fc3834456e2
arunism <thearunism@gmail.com>
2022-10-02 15:01:22 +0545

    Resnet LSTM decoder

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-# You can choose between ['adam', 'adadelta', 'adagrad', 'rmsprop', 'sgd']
-OPTIMIZER = 'adam'
 LEARNING_RATE = 0.001
 EPOCHS = 10
 BATCH_SIZE = 32
 EMBED_SIZE = 512
 HIDDEN_SIZE = 512
+DROPOUT = 0.4
 OUTPUT_PATH = 'results/'
