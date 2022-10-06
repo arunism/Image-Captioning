149061b3dbad7b7d96a7f38ca2093e7d062c4884
arunism <thearunism@gmail.com>
2022-10-07 01:10:40 +0545

    Padding

--- preprocess/vocab.py
+++ preprocess/vocab.py
@@ -1,5 +1,6 @@
 import os
 from collections import Counter
+from itertools import islice, chain, repeat
 import config
 from preprocess import CleanImageDescription
 
@@ -12,14 +13,31 @@ class Vocab:
         self.w2i = dict()  # Word to index
         self.i2w = dict()  # Index to word
         self.freq = dict()  # Word frequency
+        self.descriptions = None   # Dictionary of descriptions for each image
         self.create_vocab(clean=config.CLEAN_TEXT)
+        self.vectorize_descriptions()
 
     def create_vocab(self, clean=True):
         cid = CleanImageDescription(os.path.join(BASE_DIR, config.TEXT_DATA_PATH))
-        descriptions = cid.clean_descriptions() if clean else cid.descriptions
+        self.descriptions = cid.clean_descriptions() if clean else cid.descriptions
         self.freq = dict(Counter(
-            word for desc in descriptions.values() for sentence in desc for word in sentence.split()
+            word for desc in self.descriptions.values() for sentence in desc for word in sentence.split()
         ))
-        self.vocab = ['<PAD>', '<UNK>', '<SOS>', '<EOS>'] + sorted(self.freq.keys())
+        self.vocab = {k for k, v in self.freq.items() if v >= config.VOCAB_THRESHOLD}
+        self.vocab = ['<PAD>', '<UNK>', '<SOS>', '<EOS>'] + sorted(self.vocab)
         self.w2i = {k: v for v, k in enumerate(self.vocab)}
         self.i2w = {k: v for k, v in enumerate(self.vocab)}
+
+    # def padding(self, sequence):
+    #     return list(zip(*zip_longest(*sequence, fillvalue=self.w2i['<PAD>'])))
+
+    def padding(self, sequences):
+        sos, eos, pad = [self.w2i['<SOS>']], [self.w2i['<EOS>']], [self.w2i['<PAD>']]
+        return [
+            sos + sequence[:(config.SEQUENCE_LENGTH - 2)] + eos if len(sequence) > (config.SEQUENCE_LENGTH - 2)
+            else sos + sequence + eos + pad*(config.SEQUENCE_LENGTH - len(sequence) - 2)
+            for sequence in sequences
+        ]
+
+    def __len__(self):
+        return len(self.vocab)
