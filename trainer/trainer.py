5cae8a1f1d513fabd9e628f28358b47af662b676
arunism <thearunism@gmail.com>
2022-10-10 14:25:19 +0545

    Train func

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
 import config
 from preprocess import Vocab
 from models import ResnetModel
+from data import ImageCaptionDataset
 
 
 class Trainer:
@@ -23,3 +26,16 @@ class Trainer:
         elif config.OPTIMIZER.lower() == 'sgd':
             return torch.optim.SGD(self.model.parameters(), lr=config.LR)
         return torch.optim.Adam(self.model.parameters(), lr=config.LR)
+
+    def train_epoch(self, data_loader):
+        pass
+
+    def train(self):
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+        ])
+        data = ImageCaptionDataset(transform=transform)
+        data_loader = DataLoader(dataset=data, batch_size=config.BATCH_SIZE, shuffle=True)
+        self.model.train()
+        self.train_epoch(data_loader)
